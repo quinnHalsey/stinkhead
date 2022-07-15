9c5f964f7153c00b9cfbe5769b72c10812d31cd9
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-07-14 22:44:00 -0400

    attempt to create seed file and connect repo to database

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,8 @@
-const chalk = require("chalk");
+// const chalk = require("chalk");
 const Sequelize = require("sequelize");
-const pkg = require("../../package.json");
 
-console.log(chalk.yellow(`Opening database connection to stinkhead`));
+// console.log(chalk.yellow(`Opening database connection to stinkhead`));
+console.log("Opening database connection to stinkhead");
 
 const db = new Sequelize(`postgres://localhost:5432/stinkhead`, {
   logging: false,
--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,9 @@
 const db = require("./database");
+const Player = require("./Player");
+
+module.exports = {
+  db,
+  models: {
+    Player,
+  },
+};
